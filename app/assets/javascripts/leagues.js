7408596b1fabef68663f046fdd5fd6a079014890
github username <github email address>
2019-04-25 23:46:57 -0400

    -Addition of 2 gems -Cleared Coffeescipt comments from files -Seeded DB -Creation of League class/constructor

--- app/assets/javascripts/leagues.js
+++ app/assets/javascripts/leagues.js
@@ -1,3 +1,8 @@
-# Place all the behaviors and hooks related to the matching controller here.
-# All this logic will automatically be available in application.js.
-# You can use CoffeeScript in this file: http://coffeescript.org/
+class League {
+  constructor(name,numOfFleets,leagueAdmin) {
+    this.name = name
+    this.numOfFleets = numOfFleets
+    this.leagueAdmin = leagueAdmin
+  }
+
+}
