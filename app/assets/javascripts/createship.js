e6a3f7a29268e4bbde674e427ef217ea7142d18c
github username <github email address>
2019-04-30 02:11:10 -0400

    Ship Create Form now fully javascript frontend

--- app/assets/javascripts/createship.js
+++ app/assets/javascripts/createship.js
@@ -5,10 +5,17 @@ $('form').submit(function(event) {
   event.preventDefault();
   const values = $(this).serialize();
   const newShip = $.post('/ships', values);
-  newShip.done(function(data) {
-    debugger;
-    const ship = data;
-    $('#shipName').text(data);
+  newShip.done( (data) => {
+    if (data["id"] > 0) {
+      $('#shipMessage').text("Ship Created Successfully")
+      $('#shipName').text(data["name"]);
+      $('#shipSize').text(data["size"]);
+      $('#shipCargo').text(data["max_cargo"]);
+      $('#shipDistance').text(data["max_distance"]);
+    } else {
+      $('#shipMessage').text("Ship Creation Failed")
+      $('#shipName').text(data);
+    };
   });
   return;
 });
